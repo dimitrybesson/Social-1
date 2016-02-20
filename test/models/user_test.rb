662c867b983058369c779f447ad7bd75b960aff3
Dimitry Besson <dimitrybesson@gmail.com>
2016-02-20 14:34:47 -0500

    First commit

---
+++ test/models/user_test.rb
@@ -0,0 +1,44 @@
+require 'test_helper'
+
+#NO TABS ALLOWED IN THIS FILE
+
+class UserTest < ActiveSupport::TestCase
+	def setup
+		@user1 = users(:user1)
+		@user2 = users(:user2)
+	end
+
+  test "validates email presence" do
+    @user1.email = nil
+
+  assert_not @user1.valid?
+  end
+
+  test 'validates email uniqueness' do
+  	@user1.email = @user2.email
+
+  	assert_not @user1.valid?
+  end
+
+  test 'should follow leader' do
+  	@user1.follow!(@user2)
+
+  	assert_equal 1, @user1.leaders.count
+  	assert_equal 1, @user2.followers.count
+  end
+
+  test 'following? should be true' do
+  	@user1.follow!(@user2)
+
+  	assert @user1.following?(@user2)
+  end
+end
+
+
+
+
+
+
+# test "the truth" do
+  #   assert true
+  # end
